2d1a9e83b1445bc1bbbf34c7698b877f03fbd50b
Untitled <john.d.reddock@gmail.com>
2018-06-27 17:35:54 -0400

    create post Action Creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends  Component {
     renderField(field) {
@@ -45,6 +48,7 @@ class PostsNew extends  Component {
                 />
 
                 <button type='submit' className='btn btn-primary'>Submit</button>
+                <Link to="/" className='btn btn-danger'>Cancel</Link>
             </form>
         );
     }
@@ -74,4 +78,6 @@ function validate(values){
 export default reduxForm({
     validate, 
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost }) (PostsNew)
+);

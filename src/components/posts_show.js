925260949ef1ce6966273e2d5b1a9da070381ea6
Untitled <john.d.reddock@gmail.com>
2018-06-28 13:52:49 -0400

    posts show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Posts Show!</h2>
+            </div>
+        );
+    };
+}
+
+export default PostsShow;
